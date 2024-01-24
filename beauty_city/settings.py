9232f5e3b2d8cbb82ddad1067f5be83edb3a7102
Michael Matasyants <michael.matasyanc@gmail.com>
2024-01-25 02:32:56 +0300

    Create salon app and add models for it

--- beauty_city/settings.py
+++ beauty_city/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
     'salons',
 ]
 
@@ -102,3 +103,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     '127.0.0.1'
 ]
+
+PHONENUMBER_DEFAULT_REGION = 'RU'
